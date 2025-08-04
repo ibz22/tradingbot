4a2e2ba877fefdff3dc5980919fce81767812d08
ibz22 <ibsqutmail@gmail.com>
2025-08-03 19:50:05 -0700

    Add halalbot screening package __init__ file

---
+++ halalbot/backtest/halalbot/screening/__init__.py
@@ -0,0 +1,12 @@
+"""
+Screening utilities for assessing halal compliance.
+
+This package contains abstractions for retrieving financial statements and
+classifying them according to Islamic finance principles.  Use
+``data_gateway`` to fetch fundamental data from external providers and
+``halal_rules`` to load lists of halal approved crypto assets and
+prohibited features from configuration.
+"""
+
+from .data_gateway import DataGateway, FMPGateway  # noqa: F401
+from .halal_rules import load_rules  # noqa: F401
